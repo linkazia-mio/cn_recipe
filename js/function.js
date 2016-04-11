ec0bb8872466d0abb0a0cec90c48ad5c247da64b
mio oishi <mio.oishi@linkazia.com>
2016-04-11 11:31:21 +0900

    お気に入りボタン修正対応

--- js/function.js
+++ js/function.js
@@ -33,32 +33,32 @@ $(document).ready(function(){
     $(this).toggleClass('active');
   });
   //favorite_btn
-  $('.favorite_btn #btn_edit').click(function() {
-    $('.favorite_list dl').animate( { paddingLeft: '30px', }, 'fast' );
-    $('.favorite').addClass('edit');
-    $(".favorite_check").prop("disabled", false);
-  });
-  $('.favorite_btn #btn_cancel').click(function() {
-    $('.favorite_list dl').animate( { paddingLeft: '0', }, 'fast' );
-    $('.favorite').removeClass('edit');
-    $(".favorite_check").prop("disabled", true);
-  });
-  $('.favorite_lists .favorite_check').click(function(){
-    $(this).toggleClass('checked');
-    if($('.favorite_lists input').hasClass('checked')){
-        $('.btn_delete').show();
-        $('.btn_cancel').hide();
-    }
-    else {
-      $('.btn_delete').hide();
+    $('.favorite_btn #btn_edit').click(function() {
+      $('.favorite_list dl').animate( { paddingLeft: '30px', }, 'fast' );
+      $('.favorite').addClass('edit');
+      $(".favorite_check").prop("disabled", false);
+    });
+    $('.favorite_btn #btn_cancel').click(function() {
+      $('.favorite_list dl').animate( { paddingLeft: '0', }, 'fast' );
+      $('.favorite').removeClass('edit');
+      $(".favorite_check").prop("disabled", true);
+    });
+    $('.favorite_lists .favorite_check').click(function(){
+      $(this).toggleClass('checked');
+      if($('.favorite_lists input').hasClass('checked')){
+          $('.btn_delete').show();
+          $('.btn_cancel').hide();
+      }
+      else {
+        $('.btn_delete').hide();
+        $('.btn_cancel').show();
+      }
+    });
+    $('.favorite_btn #btn_delete').click(function(){
       $('.btn_cancel').show();
-    }
-  });
-  $('.favorite_btn #btn_delete').click(function(){
-    $('.btn_cancel').show();
-    $('.btn_delete').hide();
-    if($('.favorite_lists input').hasClass('checked')){
-      $('.favorite_lists input').removeClass('checked');
-    }
-  });
+      $('.btn_delete').hide();
+      if($('.favorite_lists input').hasClass('checked')){
+        $('.favorite_lists input').removeClass('checked');
+      }
+    });
 });
